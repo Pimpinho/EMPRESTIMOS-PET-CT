ce2edea6ed010bdb05213557c4fc3b40f7929d08
Abraão Melo <amsd@ic.ufal.br>
2025-02-10 11:23:30 -0300

    create apps

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.http import HttpResponse
+from users.views import cadastro
+from emprestimos.views import home
+           
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
+    path('cadastro/', cadastro),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views herede.
+
+def cadastro(request):
+    return HttpResponse('cadastro')
